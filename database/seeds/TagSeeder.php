4ce63df3edb50218c02baecee8f3244b6965c4a7
harbor.liu <harbor.liu@104.com.tw>
2020-04-08 10:07:53 +0800

    add TagSeeder

---
+++ database/seeds/TagSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Tag;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $records = [
+            ['name' => '音樂'],
+            ['name' => '教育'],
+            ['name' => '金融'],
+            ['name' => '證券'],
+            ['name' => '前端'],
+            ['name' => '後端'],
+            ['name' => 'UI/UX'],
+            ['name' => '農林漁牧'],
+            ['name' => '建築'],
+        ];
+        Tag::insert($records);
+    }
+}
