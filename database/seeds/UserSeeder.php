ace7742b36c709c829e3ec1c75398649249347a8
harbor.liu <harbor.liu@104.com.tw>
2020-04-13 09:25:40 +0800

    modify UserSeeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,6 +12,6 @@ class UserSeeder extends Seeder
     public function run()
     {
         factory(App\User::class, 10)->create();
-        factory(App\User::class, 20)->create();
+        factory(App\Resume::class, 20)->create();
     }
 }
