aab97f26b7cfb3d380f56e979c046ea52cc7f231
harbor.liu <harbor.liu@104.com.tw>
2020-05-06 14:46:13 +0800

    modify database.php

--- config/database.php
+++ config/database.php
@@ -147,8 +147,8 @@ return [
     'testing' => [
         'driver' => 'mysql',
         'host' => env('DB_TEST_HOST', 'localhost'),
-        'database' => env('DB_TEST_DATABASE', 'homestead'),
-        'username' => env('DB_TEST_USERNAME', 'homestead'),
+        'database' => env('DB_TEST_DATABASE', 'default'),
+        'username' => env('DB_TEST_USERNAME', 'default'),
         'password' => env('DB_TEST_PASSWORD', 'secret'),
         'charset' => 'utf8',
         'collation' => 'utf8_unicode_ci',
